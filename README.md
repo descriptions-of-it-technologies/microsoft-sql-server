6b15570a0782b8b8b198a2b3d158b33c2dcc94c3
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2021-01-19 18:51:49 +0100

    Adding new information.

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# microsoft-sql-server
+# Microsoft SQL Server.
+
+
+
+
+
+## Contents at a Glance.
+* [About](#about)
+* [Documentation.](#documentation)
+* [Pros.](#pros)
+* [Cons.](#cons)
+* [Help](#help)
+
+
+
+
+
+## About.
+`docker run -it --rm -v $PWD/entrypoint.sh:/entrypoint.sh --entrypoint=/entrypoint.sh -p 1433:1433 --name=sql mcr.microsoft.com/mssql/server:2019-latest`
+`docker exec -it sql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'P@ssword' -Q "select name from sys.databases where name = 'demo'"`
+
+
+
+
+
+## Documentation.
+
+
+
+
+
+## Pros.
+
+
+
+
+
+## Cons.
+
+
+
+
+
+## Help.
---
+++ entrypoint.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+echo "****************************************************************"
+echo "Install required"
+echo "****************************************************************"
+
+echo "****************************************************************"
+echo "Predefine variables"
+echo "****************************************************************"
+export MSSQL_PID=Developer
+export ACCEPT_EULA=Y
+export MSSQL_SA_PASSWORD=37Y5Nb8uTo2
+export TZ=Europe/Kiev
+
+function import {
+    sleep 1
+    echo "****************************************************************"
+    echo "Waiting till SQL Server is up and running"
+    echo "****************************************************************"
+    sleep 10
+    for i in {1..50}
+    do
+        /opt/mssql-tools/bin/sqlcmd -l 1 -t 1 -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -Q "SELECT @@VERSION"
+        if [ $? -eq 0 ]
+        then
+            echo "****************************************************************"
+            echo "SQL Serever started"
+            echo "****************************************************************"
+            break
+        else
+            echo "****************************************************************"
+            echo "not ready yet..."
+            echo "****************************************************************"
+            sleep 1
+        fi
+    done
+    sleep 1
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -Q "CREATE DATABASE SkarbNgoDB"
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d SkarbNgoDB -Q "CREATE LOGIN [user] WITH PASSWORD = '$MSSQL_SA_PASSWORD', CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF"
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d SkarbNgoDB -Q "CREATE USER [user] FOR LOGIN [user]"
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d SkarbNgoDB -Q "EXEC sp_addrolemember 'db_owner', 'user';"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> DONE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+}
+
+import &
+
+echo "Starting SQL Server"
+exec /opt/mssql/bin/sqlservr
